8a322af441c3e22814910558271353dc29095dea
elkin.pinto <elkin.pinto.7e7@itb.cat>
2024-05-08 12:22:57 +0200

    acs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation ("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
     implementation("org.postgresql:postgresql:42.3.1")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+    implementation(kotlin("reflect"))
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,62 +6,61 @@ import java.sql.DriverManager
 import java.sql.ResultSet
 
 
-data class Alumnos(val dni:String,val apenom:String,val direc:String,val pobla:String,val telef:String)
+data class Alumnos (val dni:String,val apenom:String,val direc:String,val pobla:String,val telef:String) {
+    constructor(): this("","","","","")
+}
 data class Assignaturas(val cod:Int, val nombre:String)
 data class Notas(val dni:String, val cod:Int, val nota:Int)
 
 fun main() {
     val jdbcUrl = "jdbc:postgresql://localhost:5432/school"
     try {
+        val pepe = DatabaseSQLController()
+        pepe.connect(jdbcUrl,"sjo","")
+        pepe.getTabla("alumnos")
+        do {
+            val alumn = pepe.next<Alumnos>()
+            println(alumn!!.dni)
+        } while(pepe.hasNext())
     } catch (e: PSQLException) {
         println(e.message)
     }
 }
 
-
-class DatabaseMONGOController {
-
-
-    fun insertData() {
-
-    }
-
-    fun insertSingledata() {
-
-    }
-
-    fun connectColection() {
-
-    }
-
-    fun connect() {
-
-    }
-}
-
-
 class DatabaseSQLController {
     private lateinit var connection:Connection
-    private lateinit var result:ResultSet
+    lateinit var result:ResultSet
 
 
     fun connect(jdbcUrl:String, user:String, password:String) {
         this.connection = DriverManager.getConnection(jdbcUrl, user, password)
     }
 
+
     fun getTabla(nomTaula:String) {
         val statement = connection.createStatement()
-        this.result = statement.executeQuery("SELECT * FROM $nomTaula'")
+        this.result = statement.executeQuery("SELECT * FROM $nomTaula")
     }
 
-    fun next(columnaType:Any, columnName:String): Any? {
-        if (this.result.next()) {
-            when (columnaType) {
-                is Int -> return this.result.getInt(columnName)
-                is String -> return this.result.getString(columnName)
-                is Float -> return this.result.getFloat(columnName)
-                is Boolean -> return this.result.getBoolean(columnName)
+    fun hasNext(): Boolean {
+        return this.result.next()
+    }
+
+    inline fun <reified T> next(): T? {
+        if (result.next()) {
+            val parametres = mutableListOf<Any>()
+            for (i in 0 until T::class.typeParameters.size) {
+                val parametre:Any = when(T::class.typeParameters[i].name) {
+                    "Int" -> result.getInt(i)
+                    "String" -> result.getString(i)
+                    "Float" -> result.getFloat(i)
+                    "Boolean" -> result.getBoolean(i)
+                    else -> ""
+                }
+                parametres.add(parametre)
             }
+
+            return T::class.constructors.first().call(parametres.toTypedArray())
         }
         return null
     }
